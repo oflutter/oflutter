5bd49317e8d5da9168cfe9027c3566ad1d005058
James Aprosail <aprosail@outlook.com>
2025-08-16 01:48:44 +0800

    feat: enhance original source gen and annotations

---
+++ oflutter_name/lib/annotation.dart
@@ -0,0 +1,58 @@
+import 'package:meta/meta.dart';
+import 'package:meta/meta_meta.dart';
+
+final _$lib = Uri(scheme: 'package', path: 'oflutter/annotate_name.dart');
+
+class GenerateNameBase {
+  const GenerateNameBase({required this.prefix});
+
+  final String prefix;
+
+  static const $prefix = 'prefix';
+}
+
+@immutable
+class TypeIdentifier {
+  const TypeIdentifier({required this.name, required this.lib});
+
+  final String name;
+  final Uri lib;
+
+  static const $name = 'name';
+  static const $lib = 'lib';
+  static final $type = TypeIdentifier(name: 'TypeIdentifier', lib: _$lib);
+
+  @override
+  bool operator ==(Object other) {
+    return other is TypeIdentifier && other.name == name && other.lib == lib;
+  }
+
+  @override
+  int get hashCode => Object.hash(name, lib);
+
+  @override
+  String toString() => '$name($lib)';
+}
+
+const name = GenerateName();
+const lib = GenerateLib();
+const type = GenerateType();
+
+class GenerateName extends GenerateNameBase {
+  const GenerateName({super.prefix = r'_$name$'});
+
+  static final $type = TypeIdentifier(name: '$GenerateName', lib: _$lib);
+}
+
+class GenerateLib extends GenerateNameBase {
+  const GenerateLib({super.prefix = r'_$lib$'});
+
+  static final $type = TypeIdentifier(name: '$GenerateLib', lib: _$lib);
+}
+
+@Target({TargetKind.classType, TargetKind.topLevelVariable})
+class GenerateType extends GenerateNameBase {
+  const GenerateType({super.prefix = r'_$type$'});
+
+  static final $type = TypeIdentifier(name: 'GenerateType', lib: _$lib);
+}
