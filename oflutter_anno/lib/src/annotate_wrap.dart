357a2af5c6562d12ec7090702042a3215066ab7c
James Aprosail <aprosail@outlook.com>
2025-08-16 01:58:41 +0800

    feat: gen names for wrap annotation

--- oflutter_anno/lib/src/annotate_wrap.dart
+++ oflutter_anno/lib/src/annotate_wrap.dart
@@ -1,8 +1,20 @@
+import 'package:oflutter_name/annotation.dart';
+
+part 'annotate_wrap.name.g.dart';
+
 const wrap = GenerateWrap();
 
+@type
 class GenerateWrap {
   const GenerateWrap({this.extensionName, this.methodName});
 
+  @name
   final String? extensionName;
+
+  @name
   final String? methodName;
+
+  static final TypeIdentifier $type = _$type$generateWrap;
+  static const String $extensionName = _$name$extensionName;
+  static const String $methodName = _$name$methodName;
 }
